7e08604b83e816d7f16b0986d50d06c40c1d7c83
rollerman111 <rollerman111@gmail.com>
2022-04-12 01:07:59 +0000

    added bubble for alerts and new property to keep track of messages seen

--- client/src/components/Sidebar/ChatContent.js
+++ client/src/components/Sidebar/ChatContent.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Typography } from "@material-ui/core";
+import { Box, Typography, Badge } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
@@ -18,23 +18,59 @@ const useStyles = makeStyles((theme) => ({
     color: "#9CADC8",
     letterSpacing: -0.17,
   },
+  alertBubble: {
+    position: 'relative',
+
+    "& .MuiBadge-anchorOriginTopRightRectangular": {
+      transform: 'none',
+    },
+    "& .MuiBadge-badge": {
+      position: 'static'
+    }
+  },
+  contentContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    overflow: 'hidden'
+  },
+  userTextContainer: {
+    overflow: 'hidden',
+    width: '100%',
+    marginRight: '5px'
+  },
+  alertedMessage: {
+    fontWeight: "bold",
+    letterSpacing: -0.2
+  }
+
+
 }));
 
 const ChatContent = ({ conversation }) => {
   const classes = useStyles();
 
-  const { otherUser } = conversation;
+  const { otherUser, notSeenCount } = conversation;
   const latestMessageText = conversation.id && conversation.latestMessageText;
+
 
   return (
     <Box className={classes.root}>
-      <Box>
+      <Box className={classes.contentContainer}>
+
+        <Box className={classes.userTextContainer}>
         <Typography className={classes.username}>
           {otherUser.username}
         </Typography>
         <Typography className={classes.previewText}>
           {latestMessageText}
         </Typography>
+        </Box>
+        {notSeenCount !== 0 ?
+          <Badge overlap="rectangular" badgeContent={notSeenCount} color="primary" className={classes.alertBubble}>
+          </Badge>
+          : <></>}
       </Box>
     </Box>
   );
