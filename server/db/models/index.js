7781c8257e812e7423d9a755b73a2bdaf7645d52
rollerman111 <rollerman111@gmail.com>
2022-04-14 08:08:34 +0000

    user

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,12 +1,12 @@
 const Conversation = require("./conversation");
 const User = require("./user");
 const Message = require("./message");
+const User_Conversation = require("./UserConversation");
 
 // associations
 
-User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
+User.belongsToMany(Conversation, { through: User_Conversation });
+Conversation.belongsToMany(User, { through: User_Conversation });
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
 
